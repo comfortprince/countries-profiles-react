0458d0ac884f0d416249574e5a27ed8b09f54923
ComfortPrince <comfort.prince.hluyo@gmail.com>
2024-03-24 20:33:09 +0200

    Completed structuring and styling the country component.

--- src/routes/countries.jsx
+++ src/routes/countries.jsx
@@ -38,9 +38,94 @@ export default function Countries() {
 			  	<Select options={ filterOptions } />
 			  </div>
 			</section>
-			<section>
-				Countries
+			<section
+				className="
+					max-md:px-6 mt-12
+					grid md:grid-cols-3 lg:grid-cols-4 gap-12
+				"
+			>
+				<Country country={{
+					imgUrl: "",
+					imgAlt: "country.flags.alt",
+					commonName: "Zimbabwe",
+					population: "1000000",
+					region: "Africa",
+					capital: "Harare"
+				}}/>
+
+				<Country country={{
+					imgUrl: "",
+					imgAlt: "country.flags.alt",
+					commonName: "Zimbabwe",
+					population: "1000000",
+					region: "Africa",
+					capital: "Harare"
+				}}/>
+
+				<Country country={{
+					imgUrl: "",
+					imgAlt: "country.flags.alt",
+					commonName: "Zimbabwe",
+					population: "1000000",
+					region: "Africa",
+					capital: "Harare"
+				}}/>
+
+				<Country country={{
+					imgUrl: "",
+					imgAlt: "country.flags.alt",
+					commonName: "Zimbabwe",
+					population: "1000000",
+					region: "Africa",
+					capital: "Harare"
+				}}/>
 			</section>
 		</>
+	)
+}
+
+function Country({country}) {
+	return (
+		<a 
+	    className="
+	    	shadow 
+	    	flex flex-col justify-between 
+	    	bg-white dark:bg-dark-blue
+	    " 
+	    href={`countries/${country.commonName}`}
+  	>
+      <img 
+        className="min-w-full h-auto rounded-t" 
+        src={country.imgUrl} 
+        alt={country.imgAlt}
+      />
+
+      <div className="bg-white dark:bg-dark-blue px-6 py-6 rounded-b">
+        <h2 className="text-lg font-bold mb-4">
+          {country.commonName}
+        </h2>
+
+        <p>
+          <span className="font-semibold">
+            Population:&nbsp;
+          </span> 
+          {country.population}
+        </p>
+
+        <p>
+          <span className="font-semibold">
+            Region:&nbsp; 
+          </span>
+          {country.region}
+        </p>
+
+        <p>
+          <span className="font-semibold">
+            Capital:&nbsp;
+          </span>
+          {country.capital}
+        </p>
+      </div>
+    </a>
 	)
 }
