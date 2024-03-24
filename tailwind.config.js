bd57951563cc696c943bdc09f084e146ddcaec76
ComfortPrince <comfort.prince.hluyo@gmail.com>
2024-03-24 16:14:52 +0200

    Setup the colors and font links

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,18 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	darkMode: 'selector',
+	theme: {
+		extend: {
+			colors: {
+				'dark-blue' : 'hsl(209, 23%, 22%)', 				// (Dark Mode Elements)
+				'very-dark-blue-dark-mode' : 'hsl(207, 26%, 17%)',	// (Dark Mode Background)
+				'very-dark-blue-light-mode' : 'hsl(200, 15%, 8%)',	// (Light Mode Text)
+				'dark-gray' : 'hsl(0, 0%, 52%)',					// (Light Mode Input)
+				'very-light-gray' : 'hsl(0, 0%, 98%)',				// (Light Mode Background)
+				'white' : 'hsl(0, 0%, 100%)',						// (Dark Mode Text & Light Mode Elements)
+			},
+		},
+	},
+	plugins: [],
 }
