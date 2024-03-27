b81a17e00c02ab0f9409031e44202c5153ad1e44
ComfortPrince <comfort.prince.hluyo@gmail.com>
2024-03-27 11:58:11 +0200

    Refactored the countries component

--- src/routes/countries.jsx
+++ src/routes/countries.jsx
@@ -1,24 +1,26 @@
 import { useState, useEffect } from 'react'
 
-import { Link } from 'react-router-dom'
+import { getCountries, hasMore } from '../countries.js'
 
-import { filterOptions } from '../constants.js'
-import { getCountries } from '../countries.js'
-
-import Select from '../components/select-dropdown'
+import CountriesSection from './sections/countries-section'
+import FiltersSection from './sections/filters-section'
 
 export default function Countries() {
 	const [countries, setCountries] = useState([])
 	const [isLoading, setIsLoading] = useState(false)
 
 	useEffect(() => {
-		let startIndex = 0, limit = 10
+		const startIndex = 0
+		let limit = 0
+
 		setIsLoading(true)
+		limit += 12
 
 		getCountries(startIndex, limit)
 		.then((countries) => {
 			setIsLoading(false)
 			setCountries(countries)
+			console.log(countries)
 		})
 		.catch((error) => {
 			setIsLoading(false)
@@ -30,8 +32,9 @@ export default function Countries() {
 			const windowHeight = document.documentElement.clientHeight
 			const scrollOffset = window.pageYOffset
 
-			if(documentHeight === windowHeight + scrollOffset) {
-				limit += 10
+			if(documentHeight === windowHeight + scrollOffset && hasMore(limit)) {
+				limit += 12
+				console.log(limit)
 				setIsLoading(true)
 
 				getCountries(startIndex, limit)
@@ -55,106 +58,14 @@ export default function Countries() {
 
 	return (
 		<>
-			<section
-				className="
-					flex flex-col md:flex-row max-md:gap-8 justify-between
-				"
-			>
-				<div
-	        role="search"
-	        className="
-	        	bg-white dark:bg-dark-blue 
-	        	w-full md:w-96 pl-6 h-12 rounded 
-	        	flex gap-4 items-center 
-	        	shadow
-	        "
-			  >
-			    	<i className="fas fa-search text-inherit text-sm"></i>
-			      <form
-			      	className="w-full h-full"
-			      >
-			      	<input
-			          role="searchbox"
-			          className="
-			          	placeholder:text-inherit bg-inherit dark:bg-inherit focus:bg-inherit outline-none
-			          	placeholder:text-sm h-full w-full rounded" 
-			          type="text"
-			          name="q" 
-			          placeholder="Search a country..."
-			        />
-			      </form>  
-			  </div>
-			  <div>
-			  	<Select options={ filterOptions } />
-			  </div>
-			</section>
-			<section
-				className="
-					max-md:px-6 mt-12
-				"
-			>
-				<div
-					className="
-						grid md:grid-cols-3 lg:grid-cols-4 gap-12
-					"
-				>
-					{countries.map((country, ndx) => (
-						<Country key={ndx} country={country}/>
-					))}
+			<FiltersSection/>
+			<CountriesSection countries={countries}/>
+
+			{isLoading && 
+				<div>
+					Loading
 				</div>
-
-				{isLoading && 
-					<div>
-						Loading
-					</div>
-				}
-			</section>
+			}
 		</>
-	)
-}
-
-function Country({country}) {
-	return (
-		<Link 
-	    className="
-	    	shadow 
-	    	flex flex-col justify-between 
-	    	bg-white dark:bg-dark-blue
-	    " 
-	    href={`countries/${country.commonName}`}
-  	>
-      <img 
-        className="min-w-full h-auto rounded-t" 
-        src={country.imgUrl} 
-        alt={country.imgAlt}
-      />
-
-      <div className="bg-white dark:bg-dark-blue px-6 py-6 rounded-b">
-        <h2 className="text-lg font-bold mb-4">
-          {country.commonName}
-        </h2>
-
-        <p>
-          <span className="font-semibold">
-            Population:&nbsp;
-          </span> 
-          {country.population}
-        </p>
-
-        <p>
-          <span className="font-semibold">
-            Region:&nbsp; 
-          </span>
-          {country.region}
-        </p>
-
-        <p>
-          <span className="font-semibold">
-            Capital:&nbsp;
-          </span>
-          {country.capital}
-        </p>
-      </div>
-    </Link>
 	)
 }
---
+++ src/routes/sections/countries-section.jsx
@@ -0,0 +1,67 @@
+import { Link } from 'react-router-dom'
+
+export default function Countries ({countries}) {
+	return (
+		<section
+			className="
+				max-md:px-6 mt-12
+			"
+		>
+			<div
+				className="
+					grid md:grid-cols-3 lg:grid-cols-4 gap-12
+				"
+			>
+				{countries.map((country, ndx) => (
+					<Country key={ndx} country={country}/>
+				))}
+			</div>
+		</section>
+	)
+}
+
+function Country({country}) {
+	return (
+		<Link 
+	    className="
+	    	shadow 
+	    	flex flex-col justify-between 
+	    	bg-white dark:bg-dark-blue
+	    " 
+	    to={`${country.commonName}`}
+  	>
+      <img 
+        className="min-w-full h-auto rounded-t" 
+        src={country.imgUrl} 
+        alt={country.imgAlt}
+      />
+
+      <div className="bg-white dark:bg-dark-blue px-6 py-6 rounded-b">
+        <h2 className="text-lg font-bold mb-4">
+          {country.commonName}
+        </h2>
+
+        <p>
+          <span className="font-semibold">
+            Population:&nbsp;
+          </span> 
+          {country.population}
+        </p>
+
+        <p>
+          <span className="font-semibold">
+            Region:&nbsp; 
+          </span>
+          {country.region}
+        </p>
+
+        <p>
+          <span className="font-semibold">
+            Capital:&nbsp;
+          </span>
+          {country.capital}
+        </p>
+      </div>
+    </Link>
+	)
+}
