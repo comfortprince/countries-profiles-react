27a5a43ca6ab3f4fc25b30d93b61b424c9d80156
Comfort <comfort.prince.hluyo@gmail.com>
2024-04-01 23:49:36 +0200

    Added a features array to the constants script

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,28 @@
 const filterOptions = [ "Filter by region", "Africa", "Asia", "America", "Europe", "Oceania"]
 
 export { filterOptions }
+
+const features = [
+	{
+		heading: 'Detailed country information',
+		content: `Discover capitals, languages, populations, geography, culture, history and more. 
+		Unleash your curiosity and explore the world.`
+	},
+	{
+		heading: 'Interactive Maps',
+		content: `Explore the world with interactive maps, uncovering geographical features, borders, landmarks
+			and terrain. Immerse yourself in the beauty of our planet.`
+	},
+	{
+		heading: 'International News',
+		content: `Stay informed with latest news and updates from across the globe. Get a global perspective
+			on current events and developments shaping our world.`
+	},
+	{
+		heading: 'International Community',
+		content: `Connect with a vibrant international community, sharing experiences, and stories.
+			Engage with fellow explorers, create content and foster a global network of knowledge.`
+	}
+]
+
+export { features }
