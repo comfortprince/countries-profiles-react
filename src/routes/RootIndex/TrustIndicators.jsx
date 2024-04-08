fbdc2955434f94fb2cccbe42db78295902d615d8
Comfort <comfort.prince.hluyo@gmail.com>
2024-04-08 14:20:14 +0200

    Added trust indicator icons svg assets. Added Trust Indicator section component to the home page

---
+++ src/routes/RootIndex/TrustIndicators.jsx
@@ -0,0 +1,12 @@
+import AirBNB from '../../assets/trust indicator icons/airbnb.svg'
+
+export default function TrustIndicators() {
+	return(
+	<>
+		<section>
+				<img src={AirBNB} className="w-10 h-10 text-red-400" />
+				
+		</section>
+	</>
+	)
+}
